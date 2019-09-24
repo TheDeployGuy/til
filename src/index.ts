2396bc38a114dd31353cc50ea409eead60753506
Jason Lloyd <jason.lloyddev@gmail.com>
2019-09-24 09:00:11 +0100

    Add css file and very basic cs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
-
-function myFunction(e) {
+function addNewTil(e) {
   e.preventDefault();
   console.log("blah");
 }
 
-document.getElementsByTagName("form")[0].addEventListener("submit", myFunction);
+document.getElementsByTagName("form")[0].addEventListener("submit", addNewTil);
