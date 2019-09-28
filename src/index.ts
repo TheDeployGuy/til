5bf00d49a0560e45d8fd25c4f4c0fc8fb98745ce
Jason Lloyd <jason.lloyddev@gmail.com>
2019-09-28 20:08:53 +0100

    Create generateNewTableRow and use it

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,27 @@ function addTil(e) {
   e.preventDefault();
   //  1. Get the table body of the table
   const tableRow = document.querySelector("tbody");
-  //  2. Create a TR for the new row
-  const newRow = document.createElement("tr");
-  //  3. Create a new TD for the date
-  const newDate = document.createElement("td");
-  //  4. Add text to the date cell
-  newDate.innerHTML = new Date().toISOString();
-  //  5. Create a new TD for the title
-  const newTitle = document.createElement("td");
-  //  6. Add text to the date cell
-  newTitle.innerHTML = "New row from JS";
-  //  7. Append both cells to row
-  newRow.appendChild(newDate);
-  newRow.appendChild(newTitle);
+  const newRow = generateNewTableRow("New Row from JS");
   //  8. Append the new row to the table
   tableRow.appendChild(newRow);
+}
+
+function generateNewTableRow(tilSummary: string) {
+  //  1. Create a TR for the new row
+  const newRow = document.createElement("tr");
+  //  2. Create a new TD for the date
+  const newDate = document.createElement("td");
+  //  3. Add text to the date cell
+  newDate.innerHTML = new Date().toISOString();
+  //  4. Create a new TD for the title
+  const newTitle = document.createElement("td");
+  //  5. Add text to the date cell
+  newTitle.innerHTML = tilSummary;
+  //  6. Append both cells to row
+  newRow.appendChild(newDate);
+  newRow.appendChild(newTitle);
+
+  return newRow;
 }
 
 document.getElementsByTagName("form")[0].addEventListener("submit", addTil);
