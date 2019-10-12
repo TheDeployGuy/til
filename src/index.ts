99307d1fe44fdfaf2547490d4d75072971404d1a
Jason Lloyd <jason.lloyddev@gmail.com>
2019-10-12 22:02:11 +0100

    Turn autocomplete off, use divs instead of table

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,22 @@ function addTil(e) {
   e.preventDefault();
   const inputVal = document.querySelector("input").value;
   //  1. Get the table body of the table
-  const tableRow = document.querySelector("tbody");
-  const newRow = generateNewTableRow(inputVal);
+  const tableRow = document.querySelector("body");
+  const newRow = generateNewTilRow(inputVal);
   //  8. Append the new row to the table
   tableRow.appendChild(newRow);
 }
 
-function generateNewTableRow(tilSummary: string) {
+function generateNewTilRow(tilSummary: string) {
   //  1. Create a TR for the new row
-  const newRow = document.createElement("tr");
+  const newRow = document.createElement("div");
+  newRow.className = "row";
   //  2. Create a new TD for the date
-  const newDate = document.createElement("td");
+  const newDate = document.createElement("time");
   //  3. Add text to the date cell
   newDate.innerHTML = new Date().toISOString();
   //  4. Create a new TD for the title
-  const newTitle = document.createElement("td");
+  const newTitle = document.createElement("p");
   //  5. Add text to the date cell
   newTitle.innerHTML = tilSummary;
   //  6. Append both cells to row
