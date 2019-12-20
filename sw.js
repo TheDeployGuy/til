e298d13deb5d258763578e39b2e56a7a2d655f8f
Jason Lloyd <jason.lloyd@jet.com>
2019-12-20 15:01:26 +0000

    Remove uneeded dependencies

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const STATIC_CACHE_NAME = "static-v1";
 
-// As soon as our sw file recieves the "install" event we cache the app-shell.
+// As soon as our sw file receives the "install" event we cache the app-shell.
 self.addEventListener("install", event => {
   console.log("[Service worker] Installing SW...", event);
   event.waitUntil(
     caches.open(STATIC_CACHE_NAME).then(cache => {
       console.log("[Service worker] Pre-caching app-shell");
-       cache.addAll(["/", "/src/index.ts", "index.html", "index.css"]);
+      cache.addAll(["/", "/src/index.js", "index.html", "index.css"]);
     })
   );
 });
