f1881124f67701c7436f012bafd8fef19bd9f991
jaslloyd <jason.lloyddev@gmail.com>
2019-12-21 00:01:52 +0000

    Cleanup old caches to not s erve old files

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // @ts-check
-// When we update a service worker for it to pick up a new version we need to increment the version number.
+// If you change something in your app (html, css etc) you need to update your service worker cache version...
 const STATIC_CACHE_NAME = "static-v1";
 
 // As soon as our sw file receives the "install" event we cache the app-shell.
@@ -8,23 +8,51 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(STATIC_CACHE_NAME).then(cache => {
       console.log("[Service worker] Pre-caching app-shell");
-      cache.addAll(["/", "/src/index.js", "index.html", "index.css"]);
+      cache.addAll([
+        "/",
+        "/src/index.js",
+        "index.html",
+        "index.css",
+        "manifest.json"
+      ]);
     })
   );
 });
 
-// The next time they visit our website, the service worker will intercept the fetch event for the site resources and if the file is found in the cache we serve that file, if we do not have the file in the cache we try fetch it. (e.g we just allow the request to pass through). This also means we need to now worry about version and cache invalidation...(TODO)
+// Only gets activated when the user closes all tabs and pages and opens up a new one, so it is the perfect place to cleanup old caches.
+self.addEventListener("activate", event => {
+  console.log("[Service Worker] Activating SW...", event);
+  event.waitUntil(
+    // This returns us the names of all the caches the browser has stored.
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== STATIC_CACHE_NAME) {
+            console.log("[Service worker] Removing old cache.", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  // Ensure SW are processed correctly as sometimes they behave weird.
+  return self.clients.claim();
+});
+
+// The next time they visit our website, the service worker will intercept the fetch event for the site resources and if the file is found in the cache we serve that file, if we do not have the file in the cache we try fetch it. (e.g we just allow the request to pass through). This also means we need to now worry about version and cache invalidation if any of html files or anything like that.
 self.addEventListener("fetch", event => {
   console.log("[Service worker] Fetching something...", event);
   // Overwrite the fetch request.
   event.respondWith(
-    // Urls are the keys to cache and event.request is the url
+    // Urls are the keys to cache and event.request is the url (this looks in all caches so we need to cleanup old caches...), could end up serving old cache files.
     caches.match(event.request).then(res => {
       // If it is cached get the cache version. otherwise try fetch it
       if (res) {
         return res;
       } else {
-        return fetch(event.request);
+        return fetch(event.request).catch(e => {
+          // Do nothing for errors for now...
+        });
       }
     })
   );
